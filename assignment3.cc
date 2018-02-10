fe1ce4bff7d9b0d50d2a37de7c3b2311d7f02bff
Samuel Piecz <z1732715@turing.cs.niu.edu>
2018-02-10 03:07:15 -0600

    Skeleton commit.

---
+++ assignment3.cc
@@ -0,0 +1,55 @@
+/***********************************************************
+ CSCI 340 - Assignment 3 - Spring 2018
+ 
+ Progammer: Sam Piecz
+ Z-ID: Z1732715
+ Section: 1
+ TA: 
+ Date Due: February 12, 2018
+ 
+ Purpose: Implement Sieve of Eratosthenes algorithm. This
+ algorithm prints all prime numbers within a range.
+ ************************************************************/
+
+/***************************************************************
+ 
+ Use: 
+
+ Parameters: 
+
+ Returns: None. 
+
+ ***************************************************************/
+void sieve( set<int>& s, const int lower, const int upper) {
+}
+
+/***************************************************************
+ 
+ Use: 
+
+ Parameters: 
+
+ Returns: None. 
+
+ ***************************************************************/
+
+void print_primes( const set<int>& s, const int lower, const int upper) {
+}
+
+/***************************************************************
+ 
+ Use: 
+
+ Parameters: 
+
+ Returns: None. 
+
+ ***************************************************************/
+void run_game(set<int>& s) {
+}
+
+int main() {
+    set<int> s;
+    run_game(s);
+    return 0;
+}
---
+++ makefile
@@ -0,0 +1,20 @@
+#
+# PROGRAM: Assign 3   
+# PROGRAMMER: Samuel Piecz 
+# LOGON ID: Z1732715 
+# DATE DUE: 02/12/18 
+#
+# Compiler variables
+CCFLAGS = -ansi -Wall -std=c++11
+
+# Rule to link object code files to create executable file
+assignment3: assignment3.o
+	g++ $(CCFLAGS) -o assignment3 assignment3.o
+
+# Rule to compile source code file to object code
+assignment3.o: assignment3.cc
+	g++ $(CCFLAGS) -c assignment3.cc
+
+# Pseudo-target to remove object code and executable files
+clean:
+	-rm -f *.o assignment3
