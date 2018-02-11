702de6bf7b62d81049928339d1dec46a1e581dd0
Samuel Piecz <z1732715@turing.cs.niu.edu>
2018-02-11 00:49:12 -0600

    Made run game and print primes. Need TODO Sieve of Eratosthenes.

--- assignment3.cc
+++ assignment3.cc
@@ -10,6 +10,15 @@
  Purpose: Implement Sieve of Eratosthenes algorithm. This
  algorithm prints all prime numbers within a range.
  ************************************************************/
+#include <iostream>
+#include <algorithm>
+#include <iomanip>
+#include <set>
+#include <string>
+using std::cin;
+using std::cout;
+using std::atoi;
+using std::setw;
 
 /***************************************************************
  Name: sieve
@@ -22,7 +31,9 @@
  Returns: None. 
 
  ***************************************************************/
-void sieve( set<int>& s, const int lower, const int upper) {
+void sieve( set<int>& s, const int lower, const int upper)
+{
+
 }
 
 /***************************************************************
@@ -36,7 +47,22 @@ void sieve( set<int>& s, const int lower, const int upper) {
  Returns: None. 
 
  ***************************************************************/
-void print_primes( const set<int>& s, const int lower, const int upper) {
+void print_primes( const set<int>& s, const int lower, const int upper) 
+{
+
+    const int ITEM_W = 4;
+    const int NO_ITEMS = 6;
+    set<int>::const_iterator it;
+    for (it = s.begin(); it != set.end(); it++)
+    {
+        cout << setw(ITEM_W) << *it;
+        if ((it - set.begin()) % NO_ITEMS == 9)
+        { 
+            cout << endl;
+        }
+    }
+
+
 }
 
 /***************************************************************
@@ -50,7 +76,48 @@ void print_primes( const set<int>& s, const int lower, const int upper) {
  Returns: None. 
 
  ***************************************************************/
-void run_game(set<int>& s) {
+void run_game(set<int>& s) 
+{
+
+    bool stopOrStart = True;
+
+    while(stopOrStart)
+    {
+
+        // instantiate strings
+        string upperInput, lowerInput;
+        int upper, lower;
+        
+        // get upper value 
+        cout << "";
+        cin >> upper;
+        if (upperInput == "q")
+        {
+            stopOrStart = False; 
+        }
+        else
+        {
+            upper = atoi(upperInput);
+        }
+        
+        // get lower value
+        cout << "";
+        cin >> lowerInput;
+        {
+            stopOrStart = False; 
+        }
+        else
+        {
+            lower = atoi(lowerInput);
+        }
+
+        // call sieve function
+        sieve(s, lower, upper); 
+
+        // call print function
+        print_primes(s, lower, upper);
+
+    }
 }
 
 int main() {
