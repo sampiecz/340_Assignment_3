c84eb7493f29c4053e1056b9c55c4580bfe39d3a
Samuel Piecz <z1732715@turing.cs.niu.edu>
2018-02-11 22:32:17 -0600

    Added .exe to makefile.

--- assignment3.cc
+++ assignment3.cc
@@ -16,6 +16,7 @@
 #include <iomanip>
 #include <set>
 #include <string>
+#include <cmath>
 
 using std::cin;
 using std::cout;
@@ -24,6 +25,7 @@ using std::stoi;
 using std::setw;
 using std::set;
 using std::string;
+using std::sqrt;
 
 /***************************************************************
  Name: sieve
@@ -38,25 +40,24 @@ using std::string;
  ***************************************************************/
 void sieve( set<int>& s, const int lower, const int upper)
 {
-    // Guessing I will need to generate all the numbers and populate them into the set?
-
     set<int>::iterator it;
 
     int m = 2;
 
-    for (it = s.begin(); it < s.end(); it++)
+    for (it = s.begin(); it != s.end(); it++)
     {
         if( *it == 1 )
         {
             continue;
         }
-        else if( *it % m == 0)
+        else if( *it % m == 0 && m <= sqrt(upper))
         {
             s.erase(*it);
         }
 
-        m++;
     } 
+
+    m++;
 }
 
 /***************************************************************
@@ -114,6 +115,7 @@ void run_game(set<int>& s)
         cout << "Please input lower bound and upper bound and hit enter (e.g. 10 100): ";
         cin >> upperAndLowerInput;
 
+        // no means no
         if (upperAndLowerInput == "q")
         {
             stopOrStart = false; 
@@ -127,6 +129,13 @@ void run_game(set<int>& s)
             int splitStringHere = upperAndLowerInput.find(" ");
             upper = upperAndLowerInput.substr(0, splitStringHere);
             lower = upperAndLowerInput.substr(splitStringHere +1, upperAndLowerInput.size());
+        }
+
+        // make that set actually have stuff in it yeah boiiii
+        for(int i = stoi(lower); i < stoi(upper); i++)
+        {
+            // waddup TA I'm inserting a number into a set
+            s.insert(i);
         }
 
         // call sieve function
--- makefile
+++ makefile
@@ -9,7 +9,7 @@ CCFLAGS = -ansi -Wall -std=c++11
 
 # Rule to link object code files to create executable file
 assignment3: assignment3.o
-	g++ $(CCFLAGS) -o assignment3 assignment3.o
+	g++ $(CCFLAGS) -o assignment3.exe assignment3.o
 
 # Rule to compile source code file to object code
 assignment3.o: assignment3.cc
@@ -17,4 +17,4 @@ assignment3.o: assignment3.cc
 
 # Pseudo-target to remove object code and executable files
 clean:
-	-rm -f *.o assignment3
+	-rm -f *.o assignment3.exe
